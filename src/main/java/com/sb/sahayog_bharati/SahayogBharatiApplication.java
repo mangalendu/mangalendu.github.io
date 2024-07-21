58e2dadf133cc4259f1b28746b69261aec5497b2
manga <manga@Mangal-PC>
2024-07-21 18:28:42 +0530

    first commit

---
+++ src/main/java/com/sb/sahayog_bharati/SahayogBharatiApplication.java
@@ -0,0 +1,12 @@
+package com.sb.sahayog_bharati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class SahayogBharatiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SahayogBharatiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sb/sahayog_bharati/SahayogBharatiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.sahayog_bharati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SahayogBharatiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
