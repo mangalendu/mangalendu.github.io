026ea5bdd8108c24715ab1588a2f80ceeece78e4
Mangalendu Jha <36084446+mangalendu@users.noreply.github.com>
2024-07-27 16:07:24 +0530

    demo controller tested ok

--- src/main/java/com/sb/sahayog_bharati/SahayogBharatiApplication.java
+++ src/main/java/com/sb/sahayog_bharati/SahayogBharatiApplication.java
@@ -2,11 +2,20 @@ package com.sb.sahayog_bharati;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class SahayogBharatiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SahayogBharatiApplication.class, args);
 	}
+	
+	@GetMapping("/hello")
+    @ResponseBody
+    public String helloWorld() {
+        return "Hello, World!";
+    }
 }
